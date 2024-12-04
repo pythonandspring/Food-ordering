d7b939da830b173de4ab38776cf414616e9d8c3f
Akash D S <akashkanekal@gmail.com>
2024-12-04 20:26:19 +0530

    Made changes and implemented swagger

--- ordering/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class OrderingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ordering'
-
-    def ready(self):
-        import ordering.signals  # Ensure signals are loaded
