dce32fd48075a93b778e682a1f5714685f229537
Akash D S <akashkanekal@gmail.com>
2024-12-08 00:43:19 +0530

    TA done

--- delivery/api/views.py
+++ delivery/api/views.py
@@ -2,17 +2,25 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from delivery.models import deliveryUser, Feedback, Contact
 from delivery.api.serializers import DeliveryUserSerializer, FeedbackSerializer, ContactSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 class DeliveryUserViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = deliveryUser.objects.all()
     serializer_class = DeliveryUserSerializer
     # permission_classes = [IsAuthenticated]  # Only authenticated users can access
 
 class FeedbackViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
 
 class ContactViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
 
--- ordering/settings.py
+++ ordering/settings.py
@@ -130,17 +130,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ],
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-
-}
-
 
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Online Food Ordering API',
